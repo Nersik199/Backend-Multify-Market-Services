cd01ec0890fc667e43d3ae7518be3c822cd7c52c
Nersik Militosyan <militosyan13@gmail.com>
2024-10-20 22:02:23 +0400

    Added API for store and product creation with validation - POST /store: Create store with file upload (logo) and validation - POST /product: Create product with multiple file upload (product images) and validation - GET /categories: Fetch all categories - GET /products/:categoryId: Fetch products by category ID

--- middleware/validate.js
+++ middleware/validate.js
@@ -1,29 +1,29 @@
-import fs from "fs/promises";
-
+import fs from 'fs/promises';
+import { v2 as cloudinary } from 'cloudinary';
 export default (schemas, target) => {
-  return (req, res, next) => {
-    const { error } = schemas.validate(req[target], {
-      abortEarly: false,
-    });
-    if (error) {
-      if (req.file) {
-        fs.unlink(req.file.path);
-      }
-      if (req.files) {
-        req.files.forEach((file) => fs.unlink(file.path));
-      }
-      const fields = {};
-      error.details.forEach((detail) => {
-        fields[detail.path[0]] = detail.message;
-      });
-      const hasErrors = Object.keys(fields).length > 0;
-      if (hasErrors) {
-        return res.status(422).json({
-          message: "Validation error",
-          fields,
-        });
-      }
-    }
-    next();
-  };
+	return async (req, res, next) => {
+		const { error } = schemas.validate(req[target], {
+			abortEarly: false,
+		});
+		if (error) {
+			if (req.file) {
+				await cloudinary.uploader.destroy(req.file.filename);
+			}
+			if (req.files) {
+				req.files.forEach(file => fs.unlink(file.path));
+			}
+			const fields = {};
+			error.details.forEach(detail => {
+				fields[detail.path[0]] = detail.message;
+			});
+			const hasErrors = Object.keys(fields).length > 0;
+			if (hasErrors) {
+				return res.status(422).json({
+					message: 'Validation error',
+					fields,
+				});
+			}
+		}
+		next();
+	};
 };
