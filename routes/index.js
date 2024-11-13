1ed4f30a09c4db1ecd55d7456a1aea68483e9567
Misha Ghalachyan <mish.ghalachyan@gmail.com>
2024-11-13 15:51:01 +0400

    write new router the index.js

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,13 @@ import { Router } from 'express';
 import superAdmin from './superAdmin.js';
 import admin from './admin.js';
 import users from './users.js';
+import cards from './cards.js';
 const router = Router();
 
 router.use('/superAdmin', superAdmin);
 router.use('/users', users);
 router.use('/admin', admin);
+router.use('/cards', cards);
 
 router.get('/home', (req, res) => {
 	res.render('index');
